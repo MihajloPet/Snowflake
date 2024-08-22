e8532508437e23720b98195f69c98fcc65c9caf7
MihajloPet <96350012+MihajloPet@users.noreply.github.com>
2024-08-22 12:48:10 +0200

    Procedure
    This is first version of procedure code used to transform and load csv files from S3 to Snowflake

--- Load_object_file.py
+++ Load_object_file.py
@@ -1,4 +1,12 @@
-# The Snowpark package is required for Python Worksheets. 
+CREATE OR REPLACE PROCEDURE BUSDATA.SF.LOAD_OBJECT_FILE("DEST_DATABASE" VARCHAR(16777216), "DEST_SCHEMA" VARCHAR(16777216), "OBJECT_NAME" VARCHAR(16777216))
+RETURNS TABLE ()
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.8'
+PACKAGES = ('snowflake-snowpark-python')
+HANDLER = 'main'
+COMMENT='this procedure will load use snowpark to get tlatest file from AWS S3 bucket salesforce-objects-data-raw, add columns if missing, add columns to Snowpark table if needed and load data to snowflake table'
+EXECUTE AS OWNER
+AS '# The Snowpark package is required for Python Worksheets. 
 # You can add more packages by selecting them using the Packages control and then importing them.
 
 import snowflake.snowpark as snowpark
@@ -8,10 +16,9 @@ from snowflake.snowpark.types import  StructType, StructField, IntegerType, Stri
 from numpy import nan
 import pandas as pd
 
-
-dest_database = 'BUSDATA'
-dest_schema = 'SF'
-object_name = 'Account'
+# dest_database = ''BUSDATA''
+# dest_schema = ''SF''
+# object_name = ''Task''
 
 def elt_s3_file(session, table_name, stage_path, object_element_file,file_to_load,table_name_ext):
     
@@ -31,10 +38,10 @@ def elt_s3_file(session, table_name, stage_path, object_element_file,file_to_loa
     #     StructField("NAME", StringType()),	
     #     StructField("SOAPTYPE", StringType())])
         
-    #     df_obj_elements = session.read.schema(schema_templ)\
-    #         .option("RECORD_DELIMITER","\n")\
-    #         .option("FIELD_DELIMITER", "\t")\
-    #         .option('SKIP_HEADER',1)\
+    #     df_obj_elements = session.read.schema(schema_templ)\\
+    #         .option("RECORD_DELIMITER","\\n")\\
+    #         .option("FIELD_DELIMITER", "\\t")\\
+    #         .option(''SKIP_HEADER'',1)\\
     #             .csv(object_element_file)
     
 
@@ -45,10 +52,10 @@ def elt_s3_file(session, table_name, stage_path, object_element_file,file_to_loa
     StructField("BYTELENGTH",	StringType()),
     StructField("AS_OF_TIMESTAMP", StringType())])
     
-    df_obj_elements = session.read.schema(schema_templ)\
-        .option("RECORD_DELIMITER","\n")\
-        .option("FIELD_DELIMITER", "\t")\
-        .option('SKIP_HEADER',1)\
+    df_obj_elements = session.read.schema(schema_templ)\\
+        .option("RECORD_DELIMITER","\\n")\\
+        .option("FIELD_DELIMITER", "\\t")\\
+        .option(''SKIP_HEADER'',1)\\
             .csv(object_element_file)
     
         
@@ -61,7 +68,7 @@ def elt_s3_file(session, table_name, stage_path, object_element_file,file_to_loa
     
     # TODO: LAMBDA IZBACUJE DATE I DATETIME KAO OBJECT, ZATO CE KREIRATI NOVE KOLONE KAO STRING, POGLEDAJ NEKI WORKAROUND
     # loop throug all rows, get names of columns, add missing columns in both snowflake table and csv file, 
-    pd_to_snowflake_dtypes = {'ID':'VARCHAR','Id':'VARCHAR','bool':'BOOLEAN','boolean':'BOOLEAN','object':'STRING','string':'STRING', 'float64':'DOUBLE','double':'DOUBLE','address':'VARCHAR','int':'INT','dateTime':'TIMESTAMP_TZ(9)', 'date':'DATE','location':'STRING'}
+    pd_to_snowflake_dtypes = {''ID'':''VARCHAR'',''Id'':''VARCHAR'',''bool'':''BOOLEAN'',''boolean'':''BOOLEAN'',''object'':''STRING'',''string'':''STRING'', ''float64'':''DOUBLE'',''double'':''DOUBLE'',''address'':''VARCHAR'',''int'':''INT'',''dateTime'':''TIMESTAMP_TZ(9)'', ''date'':''DATE'',''location'':''STRING''}
     
     
     df_obj_elements_coll = df_obj_elements.collect()
@@ -69,45 +76,45 @@ def elt_s3_file(session, table_name, stage_path, object_element_file,file_to_loa
     # for row in df_obj_elements_coll:
     for key in unique_columns_type_dict:
         # #get column name and dtype
-        column_name = key.replace('"','') #row['NAME']   
+        column_name = key.replace(''"'','''') #row[''NAME'']   
         # print(key)
-        column_dtype_pd = unique_columns_type_dict[key].replace('"','')  #row['SOAPTYPE'] #we need this so w could add columns
+        column_dtype_pd = unique_columns_type_dict[key].replace(''"'','''')  #row[''SOAPTYPE''] #we need this so w could add columns
 
-        # print(column_name, column_dtype_pd, sep=', ')
+        # print(column_name, column_dtype_pd, sep='', '')
         if column_dtype_pd==None:            
-            column_dtype_pd='string'
+            column_dtype_pd=''string''
             
         column_dtype_snowflake = pd_to_snowflake_dtypes[column_dtype_pd]
-        
+            
 
         #for test purposes, all dtypes are StringType()
         col_dtype = StringType()
         
         # # IntegerType, StringType, BooleanType, FloatType, TimestampType, DateType
-        # if (column_dtype_snowflake == 'VARCHAR') | (column_dtype_snowflake == 'STRING'):
+        # if (column_dtype_snowflake == ''VARCHAR'') | (column_dtype_snowflake == ''STRING''):
         #     col_dtype = StringType()
-        # elif column_dtype_snowflake=='DOUBLE':
+        # elif column_dtype_snowflake==''DOUBLE'':
         #     col_dtype = FloatType()
-        # elif column_dtype_snowflake=='INT':
+        # elif column_dtype_snowflake==''INT'':
         #     col_dtype = IntegerType()
-        # elif column_dtype_snowflake=='TIMESTAMP_NTZ(9)':
+        # elif column_dtype_snowflake==''TIMESTAMP_TZ(9)'':
         #     col_dtype = TimestampType()           
-        # elif column_dtype_snowflake=='DATE':
+        # elif column_dtype_snowflake==''DATE'':
         #     col_dtype = DateType()                   
-        # elif column_dtype_snowflake=='BOOLEAN':
+        # elif column_dtype_snowflake==''BOOLEAN'':
         #     col_dtype = BooleanType()          
 
         schema_fields.append(StructField(column_name, col_dtype))
-        # print(column_name,column_dtype_snowflake,col_dtype)  
+    
     
     # print(schema_fields)
     #TEMP - just for account, untill I fix things in Lambda
     # #append recordtype and as_of_timestamp
-    # if ('RecordType' not in unique_names) | ('AccountRecordType' not in unique_names) | ('RECORDTYPE' not in unique_names):
-    #     schema_fields.append(StructField('RecordType', StringType())) #StringType VariantType
+    # if (''RecordType'' not in unique_names) | (''AccountRecordType'' not in unique_names) | (''RECORDTYPE'' not in unique_names):
+    #     schema_fields.append(StructField(''RecordType'', StringType())) #StringType VariantType
     
-    # if 'as_of_timestamp' not in unique_names:
-    #     schema_fields.append(StructField('as_of_timestamp', StringType())) #TimestampType()
+    # if ''as_of_timestamp'' not in unique_names:
+    #     schema_fields.append(StructField(''as_of_timestamp'', StringType())) #TimestampType()
     
     dynamic_schema = StructType(schema_fields)
 
@@ -118,13 +125,13 @@ def elt_s3_file(session, table_name, stage_path, object_element_file,file_to_loa
     #add columns that might be missing in snowflake table but are present in csv file
     for col_name in df_csv.columns:
         if col_name not in df_table_snowflake.columns:
-            print(f'Adding columns name: {col_name}, table name: {table_name}, column dtype: {column_dtype_snowflake} column to database.')
+            print(f''Adding columns name: {col_name}, table name: {table_name}, column dtype: {column_dtype_snowflake} column to database.'')
             alter_table_sql =f"""ALTER TABLE {table_name}
                     ADD  {col_name} {column_dtype_snowflake}"""
             # run query
             session.sql(alter_table_sql).collect()
     
-            print(f'{col_name} column added to database.')
+            print(f''{col_name} column added to database.'')
     
     from snowflake.snowpark.functions import lit 
     # print(df_table_snowflake.columns)
@@ -138,12 +145,12 @@ def elt_s3_file(session, table_name, stage_path, object_element_file,file_to_loa
     for col_name in df_table_snowflake.columns:
         # print(col_name)
         if col_name not in df_csv.columns:
-            print(f'{col_name} added to dataframe')
+            print(f''{col_name} added to dataframe'')
             # try:
             #     df_csv = df_csv.with_column(col_name, lit(nan))
             # except:
             #     df_csv = df_csv.with_column(col_name, lit(pd.NaT))
-            if db_dtype_dict[col_name]=='timestamp':
+            if db_dtype_dict[col_name]==''timestamp'':
                 # df_csv = df_csv.with_column(col_name, lit(pd.NaT))
                 df_csv = df_csv.withColumn(col_name, lit(None).cast(TimestampType()))
             else:
@@ -159,72 +166,66 @@ def elt_s3_file(session, table_name, stage_path, object_element_file,file_to_loa
     # load csv fil to snowflake table
     
 
-    df_csv = df_csv.replace('None',None)
-    print('printing dtypes')
-    print(df_csv.dtypes)
-    print(df_acc_table_snowflake.dtypes)
-    
-    # # df_csv.show(20)
-    df_table_final = df_csv.write.mode('append').save_as_table(table_name_ext)
-    # # #df_table_final = df_csv.write.mode('append').save_as_table('test_acc')
+    print(df_csv.columns)
+    # df_csv.show()
+    df_table_final = df_csv.write.mode(''append'').save_as_table(table_name_ext)
+    # df_table_final = df_csv.write.mode(''append'').save_as_table(''test_acc'')
     
 
     # Return value will appear in the Results tab.
-    return df_csv #.select(col('Next_Renewal_Date__c')).dropDuplicates()
+    return df_csv
     
 
-def main(session,dest_database,dest_schema,object_name): 
+def main(session: snowpark.Session,dest_database,dest_schema,object_name): 
     # Your code goes here, inside the "main" handler.
-    # tableName = 'information_schema.packages'
-    # dataframe = session.table(tableName).filter(col("language") == 'python')
+    # tableName = ''information_schema.packages''
+    # dataframe = session.table(tableName).filter(col("language") == ''python'')
 
     # # Print a sample of the dataframe to standard output.
     # dataframe.show()
     ###################
-    print('Use')
-    session.sql(f'USE database {dest_database}')
-    session.sql(f'USE schema {dest_schema}')
+    print(''Use'')
+    session.sql(f''USE database {dest_database}'')
+    session.sql(f''USE schema {dest_schema}'')
 
     
     #Get stage name and name of file with latest timestamp
     table_name = object_name.upper()
-    table_path = f'{dest_database}.{dest_schema}'
-    table_name_ext = f'{table_path}.{table_name}'
+    table_path = f''{dest_database}.{dest_schema}''
+    table_name_ext = f''{table_path}.{table_name}''
     
     #GET STAGE NAME USED TO QUERY OBJECT
-    df_stages = session.sql('SHOW stages').collect()
+    df_stages = session.sql(''SHOW stages'').collect()
     
     # Filter stages that match the pattern *_{table_name}
-    filtered_stages = [row.name for row in df_stages if row.name and row.name.endswith(f'_{table_name}')]
+    filtered_stages = [row.name for row in df_stages if row.name and row.name.endswith(f''_{table_name}'')]
     print(filtered_stages)
     stage_name = filtered_stages[0]
     print(stage_name)
-    stage_path = f'@{table_path}.{stage_name}/'
-    object_element_file = f'{stage_path}{object_name}_-_object_elements.csv'
+    stage_path = f''@{table_path}.{stage_name}/''
+    object_element_file = f''{stage_path}{object_name}_-_object_elements.csv''
     
     
     #get name of latest updated file
-    df_stage = session.sql(f'LIST @{stage_name}').collect()
-    print('get stage')
-    max_date = pd.to_datetime('1900-01-01 00:00:00')
-    full_file_name = ''
+    df_stage = session.sql(f''LIST @{stage_name}'').collect()
+    print(''get stage'')
+    max_date = pd.to_datetime(''1900-01-01 00:00:00'')
+    full_file_name = ''''
     
     for row in range(0,len(df_stage)):
-        temp_name = df_stage[row]['name'].split("/")[-1]
-        timestamp_temp = pd.to_datetime(df_stage[row]['last_modified'])
+        temp_name = df_stage[row][''name''].split("/")[-1]
+        timestamp_temp = pd.to_datetime(df_stage[row][''last_modified''])
 
-        if (temp_name!=f'{object_name}_-_object_elements.csv') \
-            and (timestamp_temp>max_date) \
-            and df_stage[row]['name'].split('.')[1]=='csv':
+        if (temp_name!=f''{object_name}_-_object_elements.csv'') \\
+            and (timestamp_temp>max_date) \\
+            and df_stage[row][''name''].split(''.'')[1]==''csv'':
     
             
             max_date = timestamp_temp
             full_file_name = temp_name
     
-    file_to_load = f'{stage_path}{full_file_name.split("/")[-1]}'
-
-    ##
-    print('loading file: ',file_to_load,', stage_path:',stage_path)
+    file_to_load = f''{stage_path}{full_file_name.split("/")[-1]}''
+    print(''loading file: '',file_to_load)
 
     dataframe = elt_s3_file(session, table_name, stage_path, object_element_file,file_to_load,table_name_ext)
 
@@ -232,5 +233,4 @@ def main(session,dest_database,dest_schema,object_name):
     return dataframe
 
 
-def test_main(session: snowpark.Session):
-    return main(session,dest_database,dest_schema,object_name)
+    ';
